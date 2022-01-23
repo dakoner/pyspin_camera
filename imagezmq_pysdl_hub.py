eb556f244690e7fe7226147f18406f12a9ef6d97
dek <dakoner@gmail.com>
2022-01-23 17:28:28 +0000

    minor cleanups

--- imagezmq_pysdl_hub.py
+++ imagezmq_pysdl_hub.py
@@ -14,7 +14,7 @@ def run():
     image_hub = imagezmq.ImageHub()
 
     sdl2.ext.init()
-    window = sdl2.ext.Window("Hello World!", size=(1080,1440))
+    window = sdl2.ext.Window("Hello World!", size=(1440, 1080))
     window.show()
     windowSurf = sdl2.SDL_GetWindowSurface(window.window)
     windowArray = sdl2.ext.pixels3d(windowSurf.contents)
@@ -30,13 +30,14 @@ def run():
                 break
 
         rpi_name, jpg_buffer = image_hub.recv_jpg()
+        print("got image")
         image = simplejpeg.decode_jpeg( jpg_buffer, colorspace='GRAY')
-        image_hub.send_reply(b'OK')
-        repeated = numpy.repeat(image, 4, axis=2)
-        print(repeated[0][0]
-        numpy.copyto(windowArray, repeated)
+        windowArray[:,:,0:3] = image.swapaxes(0,1)
+        #repeated = numpy.repeat(image, 4, axis=2)
+        #numpy.copyto(windowArray, repeated)
 
         window.refresh()
+        image_hub.send_reply(b'OK')
 
         #timer.SDL_Delay(10)
 
